0dbfc6eef28fdc85755f2ba8364f99436eee1ca5
chirayudesai <chirayudesai@ccs.neu.edu>
2017-11-21 13:12:14 -0500

    initial maven skeleton

---
+++ VehicleRoutingOptimization/src/main/java/com/vro/VehicleRoutingOptimization/App.java
@@ -0,0 +1,13 @@
+package com.vro.VehicleRoutingOptimization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
